ba0f93420c72f6f32739a2bbd8d84ad04580ea79
RajeevBaniya <baniyarajeev811@gmail.com>
2025-12-25 15:48:52 +0530

    Added Redux store configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,22 @@
-// Redux store configuration
-// Will be implemented in Phase 3
+import { configureStore } from '@reduxjs/toolkit';
+import tokenReducer from './tokenSlice';
 
+function createStore() {
+  return configureStore({
+    reducer: {
+      token: tokenReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [],
+        },
+      }),
+  });
+}
+
+export type AppStore = ReturnType<typeof createStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
+
+export { createStore };
