542382e4ff852bb04e8095b4e7a3eea1845a3157
RajeevBaniya <baniyarajeev811@gmail.com>
2025-12-26 15:04:47 +0530

    Wrap TokenTable with ErrorBoundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 'use client';
 
 import { TokenTable } from '@/components/token-table/TokenTable';
+import { ErrorBoundary } from '@/components/system/ErrorBoundary';
+import { Header } from '@/components/layout/Header';
+import { Footer } from '@/components/layout/Footer';
+import { MobileBottomNav } from '@/components/layout/MobileBottomNav';
 import { useTokenBootstrap } from '@/hooks/useTokenBootstrap';
 
 function Home() {
   useTokenBootstrap();
 
   return (
-    <main className="min-h-screen bg-gray-900 text-white">
-      <TokenTable />
-    </main>
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col">
+      <Header />
+      <main className="flex-1 pb-16 md:pb-0">
+        <ErrorBoundary>
+          <TokenTable />
+        </ErrorBoundary>
+      </main>
+      <Footer />
+      <MobileBottomNav />
+    </div>
   );
 }
 
